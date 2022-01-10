044cf5e81c2c213f95820b5a10985b788f0b9243
Unes_h <seyed.unes@yahoo.com>
2022-01-10 21:19:12 +0330

    #3

--- Ping.py
+++ Ping.py
@@ -4,11 +4,10 @@ import platform
 def Ping(host):
     param = '-n' if platform.system().lower() == 'windows' else '-c'
     command = ['ping', param, '3', host]
-    # return subprocess.call(command)
-    return subprocess.getoutput(command)
+    return subprocess.call(command)         # return 'int',0 if it works else 1 
+    # return subprocess.getoutput(command)  # return 'str', the status of ping a host
 
-host = input('url:')
-res=Ping(host)
-# print(type(res))
-print(res)
+host = input('Host:')
+ping_st=Ping(host)
+print(ping_st)
 print('\n-----\ndone :)')
---
+++ Ping_loss_percent.py
@@ -0,0 +1,7 @@
+import subprocess ,re
+
+host = input('Host:')   # youtube.com, google.com and etc.
+ping_st=subprocess.getoutput(['ping', host])    # ping host and get the output state in 'str' 
+print(ping_st,'\n', '-'*10)          # the state of ping
+loss_percentage = re.findall(r'\d+% loss', ping_st, flags=re.IGNORECASE)[0].split('%')[0]
+print(float(loss_percentage), '\n----------\ndone :)')
