bc3c56b1ecba82e34b38880ab88083c757099e6c
Unes_h <seyed.unes@yahoo.com>
2022-01-10 21:22:55 +0330

    #3

--- Ping_loss_percent.py
+++ Ping_loss_percent.py
@@ -3,5 +3,6 @@ import subprocess ,re
 host = input('Host:')   # youtube.com, google.com and etc.
 ping_st=subprocess.getoutput(['ping', host])    # ping host and get the output state in 'str' 
 print(ping_st,'\n', '-'*10)          # the state of ping
-loss_percentage = re.findall(r'\d+% loss', ping_st, flags=re.IGNORECASE)[0].split('%')[0]
-print(float(loss_percentage), '\n----------\ndone :)')
+loss_percentage = re.findall(r'\d+% loss', ping_st, flags=re.IGNORECASE)[0].split('%')[0]   # find the loss percentage with regex
+print(float(loss_percentage), '\n', '-'*10, 'done :)')
+
